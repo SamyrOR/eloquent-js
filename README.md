8656ce3554928211602401fec34e777f3dffede9
Samyr Ribeiro <saamyr@live.com>
2021-05-11 23:16:39 -0300

    Added chapter 6 and readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+Repository to keep the exercises from eloquent js book
+
+Unfortunately i lost the first chapters exercises code, but from chapter 5 forward will guard.
---
+++ ch-6-tslo-objects.js
@@ -0,0 +1,31 @@
+
+class Vec {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+    plus(newVec) {
+      this.x += newVec.x;
+      this.y += newVec.y;
+      return this;
+    }
+  
+    minus(newVec) {
+      this.x -= newVec.x;
+      this.y -= newVec.y;
+      return this;
+    }
+  
+    get length() {
+      return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+  }
+  
+  console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+  console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+  console.log(new Vec(3, 4).length);
+  let hasOwnProperty = Symbol("hasOwnProperty");
+  let map = { one: true, two: true, hasOwnProperty: true };
+  
+  console.log(Object.prototype.hasOwnProperty.call(map, "one"));
+  
