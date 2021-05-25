346f562dbf3e2c4035aab00f65c21591fb080101
SamyrOR <saamyr@live.com>
2021-05-24 23:00:28 -0300

    Done chapter 11

---
+++ ch11-async.js
@@ -0,0 +1,48 @@
+function Promise_all(promises) {
+  return new Promise((resolve, reject) => {
+    // Your code here.
+    let result = [];
+    let count = promises.length;
+    for (let i = 0; i < promises.length; i++) {
+      //! My attempt
+      pro.then((value) => result.push(value)).catch((e) => reject(e));
+      count--;
+      if ((count = 0)) {
+        resolve(result);
+      }
+    }
+
+    //? Correct code
+    // for (let i = 0; i < promises.length; i++) {
+    //   promises[i].then(result => {
+    //     results[i] = result;
+    //     pending--;
+    //     if (pending == 0) resolve(results);
+    //   }).catch(reject);
+    // }
+  });
+}
+
+// Test code.
+Promise_all([]).then((array) => {
+  console.log("This should be []:", array);
+});
+function soon(val) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
+Promise_all([soon(1), soon(2), soon(3)]).then((array) => {
+  console.log("This should be [1, 2, 3]:", array);
+});
+Promise_all([soon(1), Promise.reject("X"), soon(3)])
+  .then((array) => {
+    console.log("We should not get here");
+  })
+  .catch((error) => {
+    if (error != "X") {
+      console.log("Unexpected failure:", error);
+    }
+  });
+
+  
