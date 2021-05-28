1bd4f330693befa37634110b44109c3bec23cf39
SamyrOR <saamyr@live.com>
2021-05-27 23:17:59 -0300

    Attempt of the last exercies from chapter 12

--- ch12-language.js
+++ ch12-language.js
@@ -48,5 +48,44 @@ console.log(parse("a # one\n   # two\n()"));
 //    operator: {type: "word", name: "a"},
 //    args: []}
 
-
 //* 12.4
+
+specialForms.set = (args, scope) => {
+  //! My Attempt with recursive
+  let name = args[0].name;
+  let backScope = Object.getPrototypeOf(scope);
+  if (Object.hasOwnProperty.call(scope, name)) {
+    let value = evaluate(args[1], scope);
+    scope[args[0].name] = value;
+    return value;
+  } else if (!scope) {
+    throw new Error();
+  } else {
+    specialForms.set(args, backScope);
+  }
+  //? Correct Answer
+  //   if (args.length != 2 || args[0].type != "word") {
+  //     throw new SyntaxError("Bad use of set");
+  //   }
+  //   let varName = args[0].name;
+  //   let value = evaluate(args[1], env);
+
+  //   for (let scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+  //     if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+  //       scope[varName] = value;
+  //       return value;
+  //     }
+  //   }
+  //   throw new ReferenceError(`Setting undefined variable ${varName}`);
+  // };
+};
+
+run(`
+do(define(x, 4),
+   define(setx, fun(val, set(x, val))),
+   setx(50),
+   print(x))
+`);
+// → 50
+run(`set(quux, true)`);
+// → Some kind of ReferenceError
